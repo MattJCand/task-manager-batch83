477144bd14191618558f6f4368804a63a0c18f55
mathieu junes <matyjunes@gmail.com>
2017-07-27 11:28:57 +0200

    add first step

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    task = Task.create(set_params)
+    redirect_to tasks_path(task)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(set_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_params
+    params.require(:task).permit(:name, :description)
+  end
+end
